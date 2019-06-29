1b5f0cbe257904d665934cbe536ad7bb11c760fe
BraeWebb <email@braewebb.com>
2019-06-30 01:52:59 +0800

    Welcome back to 2800
    Commit that should fail the tests to test the CI pipeline.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -25,5 +25,7 @@ void main() {
     // Verify that our counter has incremented.
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
+
+    fail("Is just test");
   });
 }
